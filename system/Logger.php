2180902564cac29c5a15e9730b8cdae3d9e737c4
Antony <a2020gk@outlook.com>
2024-07-29 21:05:40 +0300

    "Remove logging.json file and update Logger.php to set enabled flag based on php_sapi_name()"

--- system/Logger.php
+++ system/Logger.php
@@ -4,7 +4,7 @@ namespace System;
 class Logger {
     protected bool $enabled;
     public function __construct() {
-        $this->enabled=json_decode(file_get_contents(\CONFIG_DIR."/logging.json"),true)["enabled"];
+        $this->enabled=php_sapi_name() === "cli-server";
     }
     public function log(string $message,string $prefix="    > ") {
         if($this->enabled) error_log($prefix.$message);
